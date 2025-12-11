5d0721fd524d681fbf82717636225f1358abe20d
Ocean Ng <ocean.ng@brinks.com>
2025-12-11 15:43:42 +0800

    update cp_img.sh

--- dataset-preparing/cp_img.sh
+++ dataset-preparing/cp_img.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+# how to use:
+# e.g. bash cp_img.sh 20251211/broken_20251211 20251211/clean_20251211
 
 # Exit immediately if a command fails
 set -e
@@ -10,11 +11,12 @@ CLEAN_SRC_PATH=~/text-restoration/dataset-preparing/$2
 BROKEN_TGT_PATH=~/text-restoration/data/broken
 CLEAN_TGT_PATH=~/text-restoration/data/clean
 
-# Activate venv
-source $PROJ_PATH/venv/bin/activate
+echo ">>> Clearing old target directories...:" "$BROKEN_TGT_PATH"
+echo ">>> Clearing old target directories...:" "$CLEAN_TGT_PATH"
+rm -rf $BROKEN_TGT_PATH
+rm -rf $CLEAN_TGT_PATH
 
-# Run the Python script
-python synthetic_atm_sans-serif.py
-
-# Deactivate venv
-deactivate
+echo ">>> copying to target directories...:" "$BROKEN_TGT_PATH"
+echo ">>> copying to target directories...:" "$CLEAN_TGT_PATH"
+cp "$BROKEN_SRC_PATH"/* "$BROKEN_TGT_PATH"/
+cp "$CLEAN_SRC_PATH"/* "$CLEAN_TGT_PATH"/
