1c5b25cc45793bbfc45d4a95bf9933c0149601c0
Ocean Ng <ocean.ng@brinks.com>
2025-12-12 11:31:38 +0800

    fix: import issue

--- src/train.py
+++ src/train.py
@@ -3,7 +3,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader, random_split
-import torchvision.transforms as transforms
+# import torchvision.transforms as transforms
+import torchvision
 from PIL import Image
 import itertools
 import numpy as np
